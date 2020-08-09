883a2d175621356dbce0be3dd8b8ea70cb12d190
jisung <namgod0649@gmail.com>
2020-08-09 14:36:25 +0900

    Updates

---
+++ precache-manifest.700e06e40979ab7dd6468d50b51bd1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd258bb691fecabf0be00fd584302122",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "e2752ccd03a3cd947c2a",
+    "url": "/movie-app/static/css/main.d010d73a.chunk.css"
+  },
+  {
+    "revision": "ba52b45166b2e6f56881",
+    "url": "/movie-app/static/js/2.cf993399.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.cf993399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2752ccd03a3cd947c2a",
+    "url": "/movie-app/static/js/main.9354246e.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
