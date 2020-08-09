a3e02818865b371a6f7fce958834d356b4dcf4be
jisung <namgod0649@gmail.com>
2020-08-09 18:01:18 +0900

    css fix

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './About.css';
 
 function About(props) {
-  return <div className="about__container">about page</div>;
+  return (
+    <div className="about__container">
+      <div>Contact: namgod649@gmail.com</div>
+    </div>
+  );
 }
 
 export default About;
