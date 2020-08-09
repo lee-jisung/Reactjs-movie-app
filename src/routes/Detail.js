6c262ad4d73aa3f6991d8f11c853ec7b38e6ce39
jisung <namgod0649@gmail.com>
2020-08-09 17:33:26 +0900

    add detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Detail.css';
 
 // Route에서 전달 받은 props들을 가지고 화면에 뿌려 줄 수 있음
 class Detail extends React.Component {
@@ -11,7 +12,28 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      const data = location.state;
+      return (
+        <div className="detail_movie">
+          <img
+            src={location.state.poster}
+            alt={location.state.title}
+            title={location.state.title}
+          />
+          <div className="movie__data">
+            <h3 className="movie__title">{data.title}</h3>
+            <h5 className="movie__year">{data.year}</h5>
+            <ul className="movie__genres">
+              {data.genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="movie__summary">{data.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
